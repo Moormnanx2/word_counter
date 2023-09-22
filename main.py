249a6aad9d9be4201a5f26eebd5dcf028455ff07
Moormnanx2 <144700788+Moormnanx2@users.noreply.github.com>
2023-09-22 10:01:23 -0400

    word_count

--- main.py
+++ main.py
@@ -1,14 +1,11 @@
 # Made by Xavier Moorman en 2023, aidé par Alexandre Wilbur et Mathéo Bui
-import colorama
-def wordcounter():
-    debut_phrase = 0
-    fin_phrase = 1
-    phrase = str(input("Donnez-moi votre phrase!: "))
-    word_count = len(phrase .split())
-    print(colorama.Fore.MAGENTA + "Nombre de mots = ", word_count)
-    word_count = 0
+import colorama #J'ajoute de la couleur au programme. (Pas nécessaire, mais c'est cool)
 
-wordcounter()
+phrase = str(input("Donnez-moi votre phrase!: "))
+word_count = len(phrase .split()) 
+print(colorama.Fore.MAGENTA + "Nombre de mots = ", word_count) #Retourne le nombre de mots avec la couleur magenta
+word_count = 0 #Je rénitialise le nombre de mots à la fin du code.
+
 
 
 
